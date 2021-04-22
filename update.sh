77fd14027eb0e0ca43df482973af8113c1c1fafc
LiamMcFadden <liamm18@vt.edu>
2021-04-22 10:10:23 -0400

    update.sh

--- update.sh
+++ update.sh
@@ -6,16 +6,29 @@
 # invoke with '-e' to update external files #
 # invoke with '-r' to update repo files #
 
-update_terminal = false
-if [ $2 = "-t" ]; then
-    $update_terminal = true
-fi
-    
-if [ $1 = "-e" ]; then
-    if [ update_terminal = true ]; then
-        echo "dslkfj"
-    fi
+update_term=false
 
-elif [ $1 = "-r" ]; then
-    echo "hi"
+if [[ $2 = "-t" ]]; then
+    $update_term=true
 fi
+
+# update external files #
+if [[ $1 = "-e" ]]; then
+   cp -r ./bash_profile ./bashrc ./dircolors ./mydircolors ./tmux.conf ./vim ./vimrc ./zshrc ~/
+
+   if [[ $update_term = true ]]; then
+       cp ./settings.json /mnt/c/Users/$3/Appdata/Local/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState/settings.json 
+   fi
+
+# update repo files #
+elif [[ $1 = "-r" ]]; then
+   cp -r ~/bash_profile ~/bashrc ~/dircolors ~/mydircolors ~/tmux.conf ~/vim ~/vimrc ~/zshrc ./
+
+   if [[ $update_term = true ]]; then
+       cp /mnt/c/Users/$3/Appdata/Local/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState/settings.json ./
+   fi
+
+else
+    echo "./update [-e/-r] (-e for update external/-r for update repo) [-t] (update term settings) [win. user name]"
+fi
+
